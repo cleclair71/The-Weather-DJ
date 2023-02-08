99567a1b29eb94417ece6bc83e0023fd585ae640
JacksonR-K <jrkbirdy@hotmail.com>
2023-02-07 19:26:49 -0500

    Added playlist previews. Added default mood for unknown weather types

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,5 @@
+
+
 //variables
 var apikey = "2699b95b9f3bb5523129bfe9fb508790";
 var searchBtn = document.querySelector("#submit-selection");
@@ -10,11 +12,12 @@ var weather = document.querySelector("#weather-description");
 var currentWeather; //Initialized in displayWeather for use in getPlaylists()
 var weatherModal = document.querySelector("#myModal")
 weatherModal.style.display = "block"
-var changeCityBtn = document.querySelector("#change-city")
+var previewModal = document.querySelector("#preview-modal");
+var changeCityBtn = document.querySelector("#change-city");
 //fardina's code
 //autocomplete function using previously searched cities
 async function displayWeather(city) {
-  input.value = ""
+  input.value = "";
   //card to display playlist when city is selected
     playlist.style.display = "block";
     // fetches current weather
@@ -51,14 +54,14 @@ searchBtn.addEventListener("click", async function () {
   renderSearch()
   getPlaylists();
 });
-localStorage.clear()
+localStorage.clear();
 
 //saving searched cities
 function renderSearch(){
   var ul = document.querySelector(".city-list")
   for (var i =0; i< savedCities.length; i++) {
     var storedCities = savedCities[i]
-    var storedList = document.createElement('li')
+    var storedList = document.createElement("li")
     storedList.classList.add("stored-list")
     storedList.textContent = storedCities
     // to display weather for stored list cities
@@ -75,16 +78,9 @@ changeCityBtn.addEventListener("click", function (){
   weatherModal.style.display = "block";
 })
 
-
-
-
-
-
-    
-
 //jackson's code 
-//var key = "AIzaSyBDMCgP5fKCMZ7RcyVVZL0XPJuQuuNZqLQ" //Jackson's key
-var key = "AIzaSyCTPCZ0BW1oVO9rOTLhWPKmaxI45OKeyvA" //Hamzah's Key
+var key = "AIzaSyBDMCgP5fKCMZ7RcyVVZL0XPJuQuuNZqLQ" //Jackson's key
+//var key = "AIzaSyCTPCZ0BW1oVO9rOTLhWPKmaxI45OKeyvA" //Hamzah's Key
 
 function getPlaylists() {
   var genre = document.querySelector("#genre-dropdown").value;
@@ -104,6 +100,8 @@ function getPlaylists() {
       break;
     case "Clouds":
       mood = "Chill";
+    default:
+      mood = "Focus"
   }
 
   /* 
@@ -111,7 +109,7 @@ function getPlaylists() {
   from the user's selection to form a search along the lines of 'playlist music sad country' 
   */
   var playlistData = [];   
-    fetch ('https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&type=playlist&q=music ' + mood + ' ' + genre + '&key=' + key)
+    fetch ("https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&type=playlist&q=music " + mood + " " + genre + "&key=" + key)
       .then (function (response) {
         return response.json();
       })
@@ -127,18 +125,17 @@ function getPlaylists() {
 function showPlaylists(playlistData) {
   //Delete any children if they exist to clear previously displayed playlists
   if (playlist.children.length > 1) {
-    console.log(playlist.children.length);
     for (var i=0; i<5; i++) {
       playlist.children[1].remove();
     }
   }
   for (var i=0; i<5; i++) {
-    var playlistEl = document.createElement('div');
+    var playlistEl = document.createElement("div");
     //Create a div to contain each playlist
     playlistEl.innerHTML = 
       '<div class="playlist-card">' +
         '<h3>' + playlistData[i].snippet.title + '</h3>' +
-        '<button>Preview Playlist</button>' +
+        '<button class="preview-button" data-playlistid = ' + playlistData[i].id.playlistId + '>Preview Playlist</button>' +
       '</div>' +
       '<a href=' + 'https://www.youtube.com/playlist?list=' + playlistData[i].id.playlistId + '>' +
         '<img src=' + playlistData[i].snippet.thumbnails.high.url + '>' +
@@ -148,4 +145,40 @@ function showPlaylists(playlistData) {
     
   }
 }
+
+$(playlist).on("click", ".preview-button", function (event) {
+  getPlaylistItems(event.target.dataset.playlistid);
+});
+
+//Get each song within the playlist when user clicks to expand
+function getPlaylistItems(playlistId) {
+  fetch ("https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=" + playlistId + "&fields=items/snippet(title, thumbnails, resourceId)&key=" + key)
+    .then (function (response) {
+      return response.json();
+    })
+    .then (function (data) {
+      showPlaylistItems(data, playlistId);
+  });
+}
+
+//Display each song of a given playlist within a collapsible div
+function showPlaylistItems(playlistData, playlistid) { 
+  for (var i=0; i<Object.keys(playlistData.items).length; i++) {
+    if (playlistData.items[i].snippet.title == "Deleted video") {
+      continue;
+    }
+    console.log(playlistData.items[i]);
+    var previewContentEl = document.createElement("div");
+    previewContentEl.setAttribute("class", "modal-content");
+    previewContentEl.innerHTML = 
+      '<div>' +
+        '<h3>' + playlistData.items[i].snippet.title + '</h3>' +
+      '<a href=' + 'https://www.youtube.com/watch?v=' + playlistData.items[i].snippet.resourceId.videoId + "&list=" + playlistid + '>' +
+        '<img src=' + playlistData.items[i].snippet.thumbnails.high.url + '>' +
+      '</a>';
+
+    previewModal.appendChild(previewContentEl);
+  }
+}
 //rajvir's code
+        
